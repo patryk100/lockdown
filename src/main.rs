a77b40c8c92ea3f33b8541aee74ffd166c76b604
Patryk Perkowski <perkowskip@state.gov>
2025-12-27 21:53:45 -0500

    learning basics

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::io;
+use env_logger::{Builder, Target};
+
+fn execute_query(query: &str) {
+    log::error!("Executing query: {}", query);
+}
+
 fn main() {
-    println!("Hello, world!");
+    Builder::new()
+        .target(Target::Stdout)
+        .init();
+
+    execute_query("DROP TABLE students");
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {guess}");
 }
